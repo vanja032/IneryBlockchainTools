2d3b448cb0e937448d0197d69b6737a215caa33d
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-12-23 16:27:17 +0100

    Create FetchKeys.py

---
+++ FetchKeys.py
@@ -0,0 +1,18 @@
+import subprocess
+import json
+
+def get_keys(account):
+    public_key = json.loads(subprocess.Popen(["cline", "get", "account", account, "--json"], stdout = subprocess.PIPE).communicate()[0].decode())["permissions"][0]["required_auth"]["keys"][0]["key"]
+    keys = json.loads(subprocess.Popen(["cline", "wallet", "private_keys", "--password", "PW5KMczH1rHrLQby35BPpbRQWHoEhLtN4ScgyWYhjAaKcqzcxZv1k"], stdout = subprocess.PIPE).communicate()[0].decode())
+    for key_pair in keys:
+        if key_pair[0] == public_key:
+            print("Public key: " + key_pair[0] + "\n" + "Private key: " + key_pair[1])
+
+
+
+if __name__ == "__main__":
+    account = input("Search account name: ")
+    try:
+        get_keys(account)
+    except:
+        print("Account does not exist!")
