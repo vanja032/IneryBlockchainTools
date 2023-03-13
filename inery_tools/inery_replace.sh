f0d7b1f7a55b5cd0a619ae8eca6febacecdbb511
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2023-03-13 09:24:28 +0100

    Update inery_replace.sh

--- inery_tools/inery_replace.sh
+++ inery_tools/inery_replace.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-find ./inery.contracts -type f -readable -writable -exec sed -i "s/eosio/inery/g" {} \;
-find ./inery.contracts -type f -readable -writable -exec sed -i "s/Eosio/Inery/g" {} \;
-find ./inery.contracts -type f -readable -writable -exec sed -i "s/EOSIO/INERY/g" {} \;
-find ./inery.contracts -execdir rename 's/eosio/inery/' '{}' \+;
-find ./inery.contracts -execdir rename 's/Eosio/Inery/' '{}' \+;
-find ./inery.contracts -execdir rename 's/EOSIO/INERY/' '{}' \+;
+find ./inery.contracts -type f -readable -writable -exec sed -i "s/name1/name2/g" {} \;
+find ./inery.contracts -type f -readable -writable -exec sed -i "s/Name1/Name2/g" {} \;
+find ./inery.contracts -type f -readable -writable -exec sed -i "s/NAME1/NAME2/g" {} \;
+find ./inery.contracts -execdir rename 's/name1/name2/' '{}' \+;
+find ./inery.contracts -execdir rename 's/Name1/Name2/' '{}' \+;
+find ./inery.contracts -execdir rename 's/NAME1/NAME2/' '{}' \+;
 
-find ./inery.contracts -type f -readable -writable -exec sed -i "s/eos/ine/g" {} \;
-find ./inery.contracts -type f -readable -writable -exec sed -i "s/Eos/Ine/g" {} \;
-find ./inery.contracts -type f -readable -writable -exec sed -i "s/EOS/INE/g" {} \;
-find ./inery.contracts -execdir rename 's/eos/ine/' '{}' \+;
-find ./inery.contracts -execdir rename 's/Eos/Ine/' '{}' \+;
-find ./inery.contracts -execdir rename 's/EOS/INE/' '{}' \+;
+find ./inery.contracts -type f -readable -writable -exec sed -i "s/short_name1/short_name2/g" {} \;
+find ./inery.contracts -type f -readable -writable -exec sed -i "s/Short_name1/Short_name2/g" {} \;
+find ./inery.contracts -type f -readable -writable -exec sed -i "s/SHORT_NAME1/SHORT_NAME2/g" {} \;
+find ./inery.contracts -execdir rename 's/short_name1/short_name2/' '{}' \+;
+find ./inery.contracts -execdir rename 's/Short_name1/Short_name2/' '{}' \+;
+find ./inery.contracts -execdir rename 's/SHORT_NAME1/SHORT_NAME2/' '{}' \+;
