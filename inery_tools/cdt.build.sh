d900065a88cdcc462458a391769d83e8a1e42f2a
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2023-03-14 14:01:41 +0100

    Update cdt.build.sh

--- inery_tools/cdt.build.sh
+++ inery_tools/cdt.build.sh
@@ -1,8 +1,10 @@
-export Boost_INCLUDE_DIR="/root/inery/2.1/src/boost_1_72_0/"
+export BOOST_ROOT="/root/inery/2.1/src/boost_1_72_0/"
+echo "$BOOST_ROOT"
 
 rm -rf build/*
 mkdir build
 cd build
+echo "Build directory"
 cmake ..
 make -j 8
 
